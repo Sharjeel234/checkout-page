da42dc224274d0f91a6138dfcb85da41919d433c
Sharjeel Mansoor <91990164+Sharjeel234@users.noreply.github.com>
2022-10-20 00:18:09 +0500

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,37 @@
+
+var data = 1;
+
+document.getElementById("qt").innerText = data;
+
+
+function increment() {
+    data = data + 1;
+    document.getElementById("qt").innerText = data;
+}
+
+
+function decrement() {
+    data = data - 1;
+    document.getElementById("qt").innerText = data;
+}
+
+
+
+var data2 = 1;
+
+
+
+
+document.getElementById("qt2").innerText = data2;
+
+
+function incrementMore() {
+    data2 = data2 + 1;
+    document.getElementById("qt2").innerText = data2;
+}
+
+
+function decrementLess() {
+    data2 = data2 - 1;
+    document.getElementById("qt2").innerText = data2;
+} 
